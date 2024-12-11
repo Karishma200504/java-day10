016e874b0fc7faf7209f5ddc276fe43a400dd91a
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-11 15:07:47 +0530

    Add files via upload

---
+++ defaultconstructor.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class a{
+    int c;
+    String b;
+    a()
+    {
+        this.c=345;
+        this.b="hello amarnath";
+    }
+    void display()
+    {
+        System.out.println(b+" "+c);
+    }
+}
+public class defaultconstructor{
+    public static void main(String args[])
+    {
+        a s=new a();
+        s.display();
+    }
+} 
